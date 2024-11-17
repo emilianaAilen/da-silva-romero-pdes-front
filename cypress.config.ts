9524e66e567b1faf5b072f05f8cea3e2de058189
emilianaARomero <eromero@loopstudio.dev>
2024-11-17 00:52:47 -0300

    add cypress ci

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "besj2m",
   e2e: {
-     baseUrl: 'http://localhost:3000'
+    screenshotOnRunFailure: false,
+    baseUrl: 'http://localhost:3000'
   },
   env: {
     apiBaseUrl: 'http://localhost:8080/api',
