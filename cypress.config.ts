a7e065748ebaea3b33bfd55120c2d16a52325346
emilianaARomero <eromero@loopstudio.dev>
2024-11-17 01:00:23 -0300

    remove 2 test instances

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   projectId: "besj2m",
   e2e: {
-    screenshotOnRunFailure: false,
     baseUrl: 'http://localhost:3000'
   },
   env: {
