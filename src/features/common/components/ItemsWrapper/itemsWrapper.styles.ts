82cd1dde1a03f0c7fe4b0d681f0cb37d6acaca54
emilianaAilen <emiliana.ailen@hotmail.com>
2024-11-27 03:14:22 -0300

    add favorites for user

--- src/features/common/components/ItemsWrapper/itemsWrapper.styles.ts
+++ src/features/common/components/ItemsWrapper/itemsWrapper.styles.ts
@@ -1,9 +1,9 @@
 import { Box, styled } from "@mui/material";
 
 export const Container = styled(Box)(() => ({
-  padding: '16px',
+  marginTop: '30px',
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
-  width: '100%'
+  width: '60%',
 }));
