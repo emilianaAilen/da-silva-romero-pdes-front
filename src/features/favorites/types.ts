ff3bfef1f7994ea61d17abb34e5ab0ba4ef34dc8
emilianaAilen <emiliana.ailen@hotmail.com>
2024-11-28 17:34:22 -0300

    fix favorite image

---
+++ src/features/favorites/types.ts
@@ -0,0 +1,9 @@
+export interface Favorite {
+    id: string;
+    name: string;
+    description: string;
+    category: string;
+    price:string;
+    external_item_id: string;
+    url_image: string;
+}
